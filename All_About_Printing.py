a85cdceab42a26291ff2da3265a9985ef20966fc
DEVLOOP-S2023 <debmalya8597@gmail.com>
2026-04-11 12:55:55 +0530

    Initial commit - all about print statement and comments

---
+++ All_About_Printing.py
@@ -0,0 +1,79 @@
+print("hello world")
+print("My name is Debmalya Chaki")
+
+
+# For Single Line Comments
+
+# for multiline use ctrl + / for windows and command + / for mac
+
+print("\n") # for next line 
+
+print("hello\t world/t")
+
+print("This is a single line comment")
+# gdgdgdfvg
+# DeprecationWarn
+# dvfdgfdvgdfvgdfvgdf
+# ddsffddsfef
+
+# Print A writing about python laguage in 10000 words 
+
+print('''
+      
+Python is a versatile programming language known for its readability, simplicity, and seamless integration with other languages. Python’s extensive standard library and active community make it a popular choice for developers.
+
+In this article, we’ll focus on several areas Python is most commonly used for and highlight some of its strengths that make it so popular.
+
+Python is also considered a great coding language to start out with if you’re learning how to code. So, to help you out, we’ll recommend some online courses and share practical tips to make your coding journey both effective and enjoyable.
+
+
+What Python is used for
+From building dynamic websites to developing AI models and crawling through massive datasets, Python is incredibly versatile. It can automate repetitive tasks, dig deep into data for insights, and power software across various platforms.
+
+So, let’s take a closer look at how Python is used for web development, automation, data science, machine learning, software testing, and the gaming industry.
+
+Web development
+Python is often used to build the server side of web applications. It handles the logic, database interactions, and server configuration.
+
+It has several frameworks that simplify web development by providing pre-built modules and tools, like Django, Bottle, or Flask. They’re ideal for small applications, prototyping, and learning purposes, letting you focus on writing apps rather than the details.
+
+Developers often use Selenium, an open-source set of tools and libraries for browser automation. Selenium has an interface that lets you write test scripts in Python for web scraping dynamic content. Scrapy, an open-source and collaborative framework, is a Python-based web scraping tool used for web indexing and extracting data from websites.
+
+Finally, Python is used to create RESTful APIs, which allow different parts of a web application to communicate with each other.
+
+Here are some commonly used Python frameworks for web development:
+
+Django: A framework that enables rapid web development through its built-in features, such as the admin panel and authentication system.
+Flask: A flexible framework that gives developers more control over the components they use.
+Pyramid: A web framework that supports both small and large applications, allowing developers to start simple and scale up as needed.
+FastAPI: A high-performance web framework for building APIs with Python.
+Tornado: A framework that handles long-lived connections, making it ideal for real-time applications like WebSockets and chat systems.
+If you’re looking to get into web development with Python, you’ll want to learn the basics of HTML and CSS. Knowing a bit about Javascript and front-end libraries like React will come in useful as well.
+
+Once you’re comfortable with your skills, consider VPS hosting to deploy your Python projects using the frameworks listed above. A scalable virtual environment like a VPS offers plenty of opportunities to experiment, iterate, and showcase your work on a professional-looking website.
+
+Pro tip
+Looking for the best tools to kick-start your web application project? Hostinger's AI-powered web app maker offers an innovative, no-code solution to bring your ideas to life!
+
+Data science and machine learning
+Python libraries enable efficient data manipulation and statistical modeling in big data processing. This makes Python a suitable choice for large-scale data science projects.
+
+Additionally, Python’s data visualization libraries create detailed and interactive plots, which are essential for data analysis and reporting.
+
+Example of a visualization created with Python's Matplotlib library
+Here are some of the more popular libraries and frameworks used in machine learning:
+
+Scikit-learn: Predictive analytics and automated decision-making
+TensorFlow with the Keras API: Complex analyses and AI model development
+PyTorch: Flexibility and ease of use in research.
+Pandas and NumPy: Data manipulation and numerical computations
+These libraries come with pre-built functions and tools that simplify complex tasks, such as data preprocessing, model training, and evaluation, greatly improving productivity.
+
+If you’re not sure where to get started, you can always check out the Udemy Python for Data Science course from Jose Portilla or the blog of Wes Mckinney, the creator of Pandas. Want to jump straight into machine learning? You’ll want something that will introduce you to Scikit-learn and NumPy, like Andrew Ng’s course on Coursera.
+
+Scripting and automation
+Automation with Python involves using scripts to perform repetitive tasks without human intervention. You can automate a wide range of tasks, improving productivity and reducing errors.
+
+      
+      
+      ''')
